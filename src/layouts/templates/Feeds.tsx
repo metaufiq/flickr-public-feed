c45d1b6e0e8df27ca86dd2522d8d4846e058ae1e
Taufiq <taufiq1689@gmail.com>
2021-06-19 13:04:32 +0700

    add template Feeds

--- src/layouts/templates/Feeds.tsx
+++ src/layouts/templates/Feeds.tsx
@@ -7,37 +7,39 @@ import publicFeedsService from "../../services/publicFeedsService"
 import React from 'react';
 import FeedType from "../../config/types/domain/FeedType"
 import HomeAppBar from "../organisms/home/HomeAppBar"
-import Feeds from "../templates/Feeds"
 
 interface mainProps {
-  navigation: StackNavigationProp<any, any>;
-  route: RouteProp<any, any>;
+  feeds: FeedType[];
+  getFeeds: Function;
+  onClickFeed: Function;
 }
-const Home = (props: mainProps) => {
+const Feeds = (props: mainProps) => {
 
-  const [feeds, setFeeds] = useState([])
-  const getFeeds = async (tags: string = "") => {
-    const params = {tags}
-    const res = await publicFeedsService.list(params)
-    if (res.isError) {
-      return
-    }
 
-    setFeeds(res.items);
-  }
   
-  const toFeedDetail = async (feed: FeedType) => {
-    props.navigation.push('FeedDetail', { feed })
-  }
+
   useEffect(() => {
-    getFeeds()
+    props.getFeeds()
   }, [])
   return (
-    <Feeds
-      feeds={feeds}
-      getFeeds={getFeeds}
-      onClickFeed={toFeedDetail}
-    />
+    <View style={{ flex: 1 }}>
+      <HomeAppBar onRefresh={props.getFeeds} onSearch={props.getFeeds}></HomeAppBar>
+      <FlatGrid
+        itemDimension={150}
+        data={props.feeds}
+        style={styles.gridView}
+        spacing={5}
+        renderItem={({ item, index }: { item: FeedType, index: number }) => (
+          <TouchableOpacity onPress={() => props.onClickFeed(item)}>
+            <View style={[styles.itemContainer]}>
+              <ImageBackground source={{ uri: item?.media?.m }} style={styles.image}>
+              </ImageBackground>
+            </View>
+          </TouchableOpacity>
+
+        )}
+      />
+    </View>
   )
 }
 
@@ -56,4 +58,4 @@ const styles = StyleSheet.create({
     padding: 10,
   }
 });
-export default Home;
+export default Feeds;
